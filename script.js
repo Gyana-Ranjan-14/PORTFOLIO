c11a393009878a9f99e8255497670b5595e8610e
Gyana Ranjan Barik <gyanabarik794@gmail.com>
2022-01-06 19:29:51 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// For the animation text in first
+var typed = new Typed(".typing", {
+    strings: ["YouTuber", "Coder", "Competative Coder", "Front-End Developer", "Problem Solver"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
