f5f500403538792368ab03b06fc1599938cadb2a
Gyana Ranjan Barik <gyanabarik794@gmail.com>
2022-01-13 22:52:47 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// preloader script
+var loader = document.getElementById("preloader");
+// load event
+window.addEventListener("load", function() {
+    // to hide loader we target css
+    loader.style.display = "none";
+})
+
 // For the animation text in first
 var typed = new Typed(".typing", {
     strings: ["YouTuber", "Coder", "Competative Coder", "Front-End Developer", "Problem Solver"],
