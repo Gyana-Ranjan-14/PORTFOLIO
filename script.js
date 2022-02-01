24bc66d08bb4054ddc808e3afdd4eaf21b75c286
Gyana Ranjan Barik <gyanabarik794@gmail.com>
2022-02-01 21:22:50 +0530

    Initial commit

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ window.addEventListener("load", function() {
     loader.style.display = "none";
 })
 
+// hamburgermenu script
+const opennav = () => {
+    if (document.getElementById('mysidenav').style.width = "0px") {
+        document.getElementById('mysidenav').style.width = "250px ";
+
+    } else {
+        document.getElementById('mysidenav').style.width = "250px ";
+    }
+
+}
+const closenav = () => {
+    document.getElementById('mysidenav').style.width = "0px ";
+}
+
 // For the animation text in first
 var typed = new Typed(".typing", {
     strings: ["YouTuber", "Coder", "Competative Coder", "Front-End Developer", "Problem Solver"],
